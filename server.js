35a57cef11a54563dc91b36c85f8617fc023e683
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-09-05 01:46:56 +0100

    🐼 adding post method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   const code = `Welcome to wasteBin!
@@ -15,6 +16,11 @@ to create a new file to share with others.`;
 
 app.get("/new", (req, res) => {
   res.render("new");
+});
+
+app.post("/save", (req, res) => {
+  const value = req.body.value;
+  console.log(value);
 });
 
 app.listen(3000);
